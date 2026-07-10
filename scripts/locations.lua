43068452d95a0357c221e802f7615dc044e0e875
Chikakoo <lionel.crafts@gmail.com>
2026-07-10 00:12:13 -0500

    Added item UI/auto tracking for them; added a hub map to be used in the future; fixed dup "Level 1" section errors

--- scripts/locations.lua
+++ scripts/locations.lua
@@ -1,3 +1,5 @@
+Tracker:AddLocations("locations/Hub.jsonc")
+
 Tracker:AddLocations("locations/1-1.jsonc")
 Tracker:AddLocations("locations/1-2.jsonc")
 Tracker:AddLocations("locations/1-3.jsonc")
@@ -6,5 +8,7 @@ Tracker:AddLocations("locations/1-5.jsonc")
 Tracker:AddLocations("locations/1-6.jsonc")
 Tracker:AddLocations("locations/1-7.jsonc")
 Tracker:AddLocations("locations/1-8.jsonc")
+--Tracker:AddLocations("locations/1-9.jsonc")
+--Tracker:AddLocations("locations/1-O.jsonc")
 
 Tracker:AddLocations("locations/Level2.jsonc")
