d7b5cdeb7493c0642bcb36e59dd089934df237d7
MECHBOT\ytroy <birchshadowsme@gmail.com>
2026-07-10 20:00:23 -0500

    Updated map image and added item locations for 1-9 (catwalks)

--- scripts/locations.lua
+++ scripts/locations.lua
@@ -8,7 +8,7 @@ Tracker:AddLocations("locations/1-5.jsonc")
 Tracker:AddLocations("locations/1-6.jsonc")
 Tracker:AddLocations("locations/1-7.jsonc")
 Tracker:AddLocations("locations/1-8.jsonc")
---Tracker:AddLocations("locations/1-9.jsonc")
+Tracker:AddLocations("locations/1-9.jsonc")
 --Tracker:AddLocations("locations/1-O.jsonc")
 
 Tracker:AddLocations("locations/Level2.jsonc")
